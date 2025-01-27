70fa36b3698ea3fd67499884392b963bea177176
Zmylife <164553179+Zmylife@users.noreply.github.com>
2025-01-28 01:08:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,24 +5,24 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
-    SUDO_NB: process.env.SUDO_NB || "923237045919",
+    SESSION_ID: process.env.SESSION_ID || "NHNS2BJY#xG-8pJvH3DY9aczHc5VlHlJWBDM0F25ZkaOGTFpfTjY",
+    SUDO_NB: process.env.SUDO_NB || "923370624968",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
     MODE: process.env.MODE || "public",
-    AUTO_VOICE: process.env.AUTO_VOICE || "false",
-    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "true",
     AUTO_REPLY: process.env.AUTO_REPLY || "true",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/fcjrrk.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ArslanMD AND MY BOT IS ONLINE I'M Arslan-MD WHATSAPP BOT 😊♻️",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/fcjrrk.jpg"
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM zaniimd AND MY BOT IS ONLINE I'M zanii-MD WHATSAPP BOT 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
-    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
     AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "true",
     OWNER_REACT: process.env.OWNER_REACT || "true",
-    BOT_NAME: process.env.BOT_NAME || "Arslan-MD࿐",
+    BOT_NAME: process.env.BOT_NAME || "Zain Khosa࿐",
     GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
     PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
